efaf82794e13b96dd587bdf852501ce6b1d02ccf
juanivalle <juanignaciovalle18@gmail.com>
2023-02-23 07:46:23 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -29,3 +29,17 @@ class BaseModel:
         dicc["created_at"] = self.created_at.isoformat()
         dicc["updated_at"] = self.updated_at.isoformat()
         return dicc
+
+    def __init__(self, *args, **kwargs):
+        """comments"""
+
+        if **kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.datetime.now()
+                    self.updated_at = self.created_at
+
+
