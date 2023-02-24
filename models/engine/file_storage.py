bcf39405535f931804ebd9b814bad9561d886cc9
juanivalle <juanignaciovalle18@gmail.com>
2023-02-24 07:47:05 -0800

    task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""comments"""
+
+
+from models.base_model import BaseModel
+import json
+
+
+class FileStorage:
+    """comments"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """comments"""
+
+        return self.__objects
+
+    def new(self, obj):
+        """comments"""
+
+        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """comments"""
+
+        with open(self.__file_path, 'w') as f:
+            objects_json = {}
+            for key, value in self.__objects.items():
+                objects_json[key] = value.to_dict()
+            json.dump(objects_json, f)
+
+    def reload(self):
+        """comments"""
+
+        try:
+            with open(self.__file_path, 'r') as d:
+                objects_json = json.load(d)
+                for key in objects_json:
+                    self.__objects[key] = classes[objects_json[key]["__class__"]](**objects_json[key])
+        except FileNotFoundError:
+            pass
