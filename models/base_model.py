c3910b2cc236428aa9a9719be8bdf88d9d723091
washx23 <5731@holbertonstudents.com>
2023-02-24 17:13:03 -0800

    fix task 5

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 
 import datetime
 import uuid
-from engine import file_storage
 
 class BaseModel:
     """comments"""
@@ -21,7 +20,7 @@ class BaseModel:
         """comments"""
 
         self.updated_at = datetime.datetime.now()
-        file_storage.save()
+        storage.save()
     
     def to_dict(self):
         """comments"""
@@ -41,4 +40,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            file_storage.new(self)
+            storage.new(self)
+
+    
