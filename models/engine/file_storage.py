948ff2b545973de3b64c750fe1ea979ba0065e69
washx23 <5731@holbertonstudents.com>
2023-02-24 18:45:06 -0800

    fix 5.02

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@
 
 import json
 from models.base_model import BaseModel
+import os.path
 
 
 class FileStorage:
@@ -34,13 +35,13 @@ class FileStorage:
 
     def reload(self):
         """comments"""
-
+        dicc1 = {}
+        
         try:
             with open(FileStorage.__file_path, mode='r', encoding='utf-8') as f:
-                objects_json = json.load(f)
-                for key, value in objects_json.items():
-                    obj_class = value['__class__']
-                    obj = eval(obj_class + "(**value)")
-                    FileStorage.__objects[key] = obj
+                for key, value in json.load(f).items():
+                    mt = str(key).split(".")
+                    if mt[0] in dicc1.keys():
+                        self.__objects[key] = dicc1[mt[0]](**value)
         except FileNotFoundError:
             pass
