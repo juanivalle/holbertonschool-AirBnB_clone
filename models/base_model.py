73db9b7916c11f5715b6582d02259f29de6c9ac5
washx23 <5731@holbertonstudents.com>
2023-02-24 17:29:13 -0800

    fix task 5.01

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
         else:
             for key, value in kwargs.items():
