ca72dcd8a3d02f8029ccd3da6e7b34fa9c2e40e9
juanivalle <juanignaciovalle18@gmail.com>
2023-02-27 06:24:21 -0800

    push for pull

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 """comments"""
 
-import datetime
-import uuid
+from datetime import datetime
+from uuid import uuid4
+import models
 
 class BaseModel:
     """comments"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
-    
+    def __init__(self, *args, **kwargs):
+        """ Construct """
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
     def __str__(self):
         """comments"""
         return(f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
@@ -19,27 +37,16 @@ class BaseModel:
     def save(self):
         """comments"""
 
-        self.updated_at = datetime.datetime.now()
-        storage.save()
+        self.updated_at = datetime.now()
+        models.storage.save()
     
     def to_dict(self):
         """comments"""
 
-        dicc = self.__dict__.copy()
-        dicc["__class__"] = type(self).__name__
-        dicc["created_at"] = self.created_at.isoformat()
-        dicc["updated_at"] = self.updated_at.isoformat()
-        return dicc
-
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-            storage.new(self)
-
-    
+    def to_dict(self):
+        """ Return a dictonary """
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
